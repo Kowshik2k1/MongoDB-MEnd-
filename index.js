dc60d5729a799b5c946d528cb8da1f858f8bff7b
Kowshik2k1 <koushikvarmabalaraju369@gmail.com>
2025-07-08 23:40:22 +0530

    Module End Assignment for MongoDB

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const userRoutes = require("./routes/UserRoutes");
+require('dotenv').config();
+
+// Middleware setup
+app.use(express.json());
+
+// Connect to MongoDB Atlas (Replace <your_connection_string>)
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.error("Connection Error:", err));
+
+// Use routes
+app.use("/api", userRoutes);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running in ${process.env.PORT}`)
+})
---
+++ routes/UserRoutes.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const User = require("../models/User");
+const router = express.Router();
+
+// Add a new user
+router.post("/users", async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// Inital message
+router.get("/", (req, res) => {
+    res.send("Hello! Welcome to the server");
+});
+
+// Get all users
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Update a user by ID
+router.put("/users/:id", async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedUser) return res.status(404).json({ message: "User not found" });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Remove a user by ID
+router.delete("/users/:id", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
